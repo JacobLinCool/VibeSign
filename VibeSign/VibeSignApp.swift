6e621182263f3bac9d38adf2c534b910c0345482
JacobLinCool <jacoblincool@gmail.com>
2025-05-14 01:59:38 +0800

    Initial Commit

---
+++ VibeSign/VibeSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeSignApp.swift
+//  VibeSign
+//
+//  Created by Jacob Lin on 2025/5/14.
+//
+
+import SwiftUI
+
+@main
+struct VibeSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeSignTests/VibeSignTests.swift
@@ -0,0 +1,16 @@
+//
+//  VibeSignTests.swift
+//  VibeSignTests
+//
+//  Created by Jacob Lin on 2025/5/14.
+//
+
+import Testing
+
+struct VibeSignTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
